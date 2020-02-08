dccc761c678281e3cde56b6b9e181dc772350903
Pierre <pierre.albrecht@web.de>
2020-02-08 22:29:22 +0100

    Now synchronises after reloading

--- test.js
+++ test.js
@@ -4,6 +4,8 @@ var io = require('socket.io')(http);
 const jsdom = require("jsdom");
 const {JSDOM} = jsdom;
 
+
+/*
 const dom = new JSDOM(`<!DOCTYPE html><body><canvas id="canvas"></canvas><p>Hello world</p></body>`);
 console.log(dom.window.document.querySelector("p").textContent); // "Hello world"
 var canvas = dom.window.document.createElement('canvas');
@@ -13,7 +15,52 @@ canvas.height = 768;
 canvas.style.zIndex = 8;
 canvas.style.position = "absolute";
 canvas.style.border = "1px solid";
+*/
+const DrawingType = {Dot: 1, Line: 2};
+class DrawingStep {
+    lineWidth = 10;
+    lineCap = "butt";
+    strokeStyle = "black";
+    posX = 0;
+    posY = 0;
 
+    constructor(posX, posY, lineWidth = 10, lineCap = "butt", strokeStyle = "black") {
+        this.posX = posX;
+        this.posY = posY;
+        this.lineWidth = lineWidth;
+        this.lineCap = lineCap;
+        this.strokeStyle = strokeStyle;
+    }
+
+    get posX() {
+        return this.posX;
+    }
+
+    get posY() {
+        return this.posY;
+    }
+
+    SendToClient (socket) {
+        socket.emit('new dot', this.posX, this.posY);
+    }
+}
+
+class DrawingStepLine extends DrawingStep {
+    lastX = 0;
+    lastY = 0;
+
+    constructor(lastX, lastY, posX, posY, lineWidth = 10, lineCap = "butt", strokeStyle = "black") {
+        super(posX,posY,lineWidth,lineCap,strokeStyle);
+        this.lastX = lastX;
+        this.lastY = lastY;
+    }
+
+    SendToClient (socket) {
+        socket.emit('new line', this.lastX, this.lastY, this.posX, this.posY);
+    }
+}
+
+var DrawingStack = [];
 
 http.listen(3000, function() {
     console.log('Server gestartet, listening on localhost:3000');
@@ -34,9 +81,21 @@ io.on('connection', function (socket, name) {
         console.log('Nachricht erhalten.');
         io.emit('chat message', msg);
     });
-    socket.on('line added', function(xPos, yPos) {
-        console.log('Line was added.');
-        io.emit('new line', xPos, yPos);
+    socket.on('line added', function(lastX, lastY, xPos, yPos) {
+        DrawingStack.push(new DrawingStepLine(lastX,lastY,xPos,yPos));
+        console.log('Line was added. Drawing Stack: ', DrawingStack.length);
+        io.emit('new line', lastX, lastY, xPos, yPos);
+    });
+    socket.on('dot added', function(xPos,yPos) {
+        DrawingStack.push(new DrawingStep(xPos,yPos));
+        console.log('Dot was added. Drawing Stack: ', DrawingStack.length);
+        io.emit('new dot', xPos, yPos);
+    })
+    socket.on('request drawing data', function() {
+        DrawingStack.forEach(element => {
+            element.SendToClient(socket);
+        });
+        socket.emit('drawing data end');
     });
 });
 
